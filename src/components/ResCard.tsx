b052ddbdc455f2d667dd969d6b501cc3284a2fa6
Dustella <fdnoaivj@outlook.com>
2023-06-30 22:20:38 +0800

    ✨ list spa

--- src/components/ResCard.tsx
+++ src/components/ResCard.tsx
@@ -3,18 +3,20 @@ import type { DataResource } from '../types/Res'
 
 const ResCard: Component<DataResource> = (props) => {
   const card = (
-    <div class="border shadow-sm z-1 py-7 relative bg-white w-[330px] h-md  p-4 hover:shadow-xl transition duration-300">
-      <h2 class="font-bold">{props.title}</h2>
-      <p class="px-2 py-1 bg-gray-300 text-black inline-block text-sm my-2 rounded-xl">
-        {props.l1_title}
-      </p>
-      <a
-        href={props.href}
-        target="_blank"
-        class="transition duration-300 text-end absolute right-2 bottom-4 font-bold px-4 bg-[#171717] rounded-lg text-sm text-white py-2 border hover:bg-white hover:text-black hover:border-black"
-      >
-        下载
-      </a>
+    <div class="basis-1/3 p-2">
+      <div class="my-3  border shadow-sm z-1 py-7 relative bg-white  h-[170px]  p-4 hover:shadow-xl transition duration-300">
+        <h2 class="font-bold">{props.title}</h2>
+        <p class="px-2 py-1 bg-gray-300 text-black inline-block text-sm my-2 rounded-xl">
+          {props.l1_title}
+        </p>
+        <a
+          href={props.href}
+          target="_blank"
+          class="transition duration-300 text-end absolute right-2 bottom-4 font-bold px-4 bg-[#171717] rounded-lg text-sm text-white py-2 border hover:bg-white hover:text-black hover:border-black"
+        >
+          下载
+        </a>
+      </div>
     </div>
   )
 
--- src/components/ResSection.tsx
+++ src/components/ResSection.tsx
@@ -3,12 +3,50 @@ import type { DataResource } from '../types/Res'
 import ResCard from './ResCard'
 
 const ResList: Component<{ list: DataResource[] }> = (props) => {
-  const list = (
-    <div class="flex flex-wrap container z-1 items-center relative  justify-between mx-auto">
-      <For each={props.list}>{(res) => <ResCard {...res} />}</For>
+  const target = {} as {
+    // index string
+    [key: string]: {
+      [key: string]: DataResource[]
+    }
+  }
+
+  // eslint-disable-next-line solid/reactivity
+  props.list.forEach((item) => {
+    if (!target[item.l1_title]) {
+      target[item.l1_title] = {}
+    }
+    if (!target[item.l1_title][item.l2_title]) {
+      target[item.l1_title][item.l2_title] = []
+    }
+    target[item.l1_title][item.l2_title].push(item)
+  })
+
+  const res = (
+    <div class="max-w-[1080px] mx-auto">
+      <For each={Object.entries(target)}>
+        {([l1title, line]) => {
+          return (
+            <>
+              <h1 class="text-2xl font-bold my-6">{l1title}</h1>
+              {Object.entries(line).map(([l2title, items]) => {
+                return (
+                  <>
+                    <div class="w-[1080px]">
+                      <h2 class="text-xl">{l2title}</h2>
+                      <div class="flex  flex-wrap auto-cols-min container z-1 items-start relative  mx-auto">
+                        <For each={items}>{(res) => <ResCard {...res} />}</For>
+                      </div>
+                    </div>
+                  </>
+                )
+              })}
+            </>
+          )
+        }}
+      </For>
     </div>
   )
-  return list
+  return res
 }
 
 export default ResList
