ba91e1dad4e77d57f3bab2b1edaae539fd06b29c
Dustella <fdnoaivj@outlook.com>
2023-06-30 17:51:35 +0800

    🚑

--- src/components/ResCard.tsx
+++ src/components/ResCard.tsx
@@ -3,10 +3,18 @@ import type { DataResource } from '../types/Res'
 
 const ResCard: Component<DataResource> = (props) => {
   const card = (
-    <div class="border shadow-sm z-1 relative bg-white w-1/4 h-md m-4 p-4 hover:shadow-xl transition duration-300">
-      <h2>{props.title}</h2>
-      <p>{props.l1_title}</p>
-      <a href={props.href}>Learn more</a>
+    <div class="border shadow-sm z-1 py-7 relative bg-white w-[330px] h-md  p-4 hover:shadow-xl transition duration-300">
+      <h2 class="font-bold">{props.title}</h2>
+      <p class="px-2 py-1 bg-gray-300 text-black inline-block text-sm my-2 rounded-xl">
+        {props.l1_title}
+      </p>
+      <a
+        href={props.href}
+        target="_blank"
+        class="transition duration-300 text-end absolute right-2 bottom-4 font-bold px-4 bg-[#171717] rounded-lg text-sm text-white py-2 border hover:bg-white hover:text-black hover:border-black"
+      >
+        下载
+      </a>
     </div>
   )
 
--- src/components/ResSection.tsx
+++ src/components/ResSection.tsx
@@ -4,7 +4,7 @@ import ResCard from './ResCard'
 
 const ResList: Component<{ list: DataResource[] }> = (props) => {
   const list = (
-    <div class="flex flex-wrap container z-1 items-center relative  justify-center mx-auto">
+    <div class="flex flex-wrap container z-1 items-center relative  justify-between mx-auto">
       <For each={props.list}>{(res) => <ResCard {...res} />}</For>
     </div>
   )
