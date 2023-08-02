3fac6c29c3e7dfa2cd32648551b7d1218b19fff0
Dustella <fdnoaivj@outlook.com>
2023-08-02 19:38:17 +0800

    ✨ load fonts async

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,16 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          rel: 'stylesheet',
+          rel: 'preload',
+          as: 'style',
+          onload: 'this.onload=null;this.rel=\'stylesheet\'',
           href: 'https://testingcf.jsdelivr.net/npm/@chinese-fonts/syst@1.3.0/dist/SourceHanSerifCN/result.css',
+
         },
         {
-          rel: 'stylesheet',
+          rel: 'preload',
+          as: 'style',
+          onload: 'this.onload=null;this.rel=\'stylesheet\'',
           href: 'https://testingcf.jsdelivr.net/npm/@chinese-fonts/ysbth@1.1.0/dist/优设标题黑/result.css',
         },
       ],
